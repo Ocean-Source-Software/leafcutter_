c4d702939695deaead6bbe58046fd62894d6ddfe
Ulugbek Baymuradov <ulugbekbk@gmail.com>
2023-02-04 18:24:07 +0500

    mamba in build.sh test

--- conda_recipe/build.sh
+++ conda_recipe/build.sh
@@ -9,5 +9,5 @@ grep -v '^Priority: ' DESCRIPTION.old >DESCRIPTION
 
 yes | mamba install r-base r-bh r-dt r-hmisc r-r.utils r-rcpp r-rcppeigen r-stanheaders r-domc r-dplyr r-foreach r-ggplot2 r-gridextra r-intervals r-optparse r-reshape2 r-rstan r-shiny r-shinycssloaders r-roxygen2 bioconductor-dirichletmultinomial
 
-$R CMD INSTALL --preclean --build .
+R CMD INSTALL --preclean --build .
 popd
