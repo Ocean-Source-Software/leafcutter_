8c23971f7c43bd5260300feb6b0a17c5236598e1
Ulugbek Baymuradov <ulugbekbk@gmail.com>
2023-02-06 16:41:59 +0500

    more bash tests

--- conda_recipe/build.sh
+++ conda_recipe/build.sh
@@ -7,7 +7,13 @@ pushd ./leafcutter
 mv DESCRIPTION DESCRIPTION.old
 grep -v '^Priority: ' DESCRIPTION.old >DESCRIPTION
 
-yes | mamba install r-base r-bh r-dt r-hmisc r-r.utils r-rcpp r-rcppeigen r-stanheaders r-domc r-dplyr r-foreach r-ggplot2 r-gridextra r-intervals r-optparse r-reshape2 r-rstan r-shiny r-shinyjs r-shinycssloaders r-roxygen2 bioconductor-dirichletmultinomial r-rstantools r-essentials
+yes | mamba install r-base r-bh r-dt r-hmisc r-r.utils r-rcpp r-rcppeigen r-stanheaders r-domc r-dplyr r-foreach r-ggplot2 r-gridextra r-intervals r-optparse r-reshape2 r-rstan r-shiny r-shinyjs r-shinycssloaders r-roxygen2 bioconductor-dirichletmultinomial r-rstantools r-essentials samtools
+Rscript -e 'if (!require("BiocManager", quietly = TRUE))
+    install.packages("BiocManager", repos="http://cran.us.r-project.org")
 
-R CMD INSTALL --preclean --build .
+BiocManager::install("Biobase")
+install.packages("TailRank", repos="http://R-Forge.R-project.org")
+'
+
+R CMD INSTALL --build .
 popd
