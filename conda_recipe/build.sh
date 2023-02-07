aefc9f7cee980d0da6e85e8929ede3f315eae5b7
Ulugbek Baymuradov <ulugbekbk@gmail.com>
2023-02-07 21:56:10 +0500

    devtools/bash install

--- conda_recipe/build.sh
+++ conda_recipe/build.sh
@@ -2,18 +2,11 @@
 
 set -e -x
 
-pushd ./leafcutter
-# R refuses to build packages that mark themselves as Priority: Recommended
-mv DESCRIPTION DESCRIPTION.old
-grep -v '^Priority: ' DESCRIPTION.old >DESCRIPTION
-
-yes | mamba install r-base r-bh r-dt r-hmisc r-r.utils r-rcpp r-rcppeigen r-stanheaders r-domc r-dplyr r-foreach r-ggplot2 r-gridextra r-intervals r-optparse r-reshape2 r-rstan r-shiny r-shinyjs r-shinycssloaders r-roxygen2 bioconductor-dirichletmultinomial r-rstantools r-essentials samtools
+yes | mamba install r-devtools samtools
 Rscript -e 'if (!require("BiocManager", quietly = TRUE))
     install.packages("BiocManager", repos="http://cran.us.r-project.org")
 
 BiocManager::install("Biobase")
-install.packages("TailRank", repos="http://R-Forge.R-project.org")
+BiocManager::install("DirichletMultinomial")
+devtools::install_github("davidaknowles/leafcutter/leafcutter")
 '
-
-R CMD INSTALL --build .
-popd
