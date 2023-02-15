3a8d601dbf82890818f385d72edd5da77b894c5c
Ulugbek Baymuradov <ulugbekbk@gmail.com>
2023-02-15 21:55:42 +0400

    save changes after merge

--- conda_recipe/build.sh
+++ conda_recipe/build.sh
@@ -2,6 +2,11 @@
 
 set -e -x
 
+mkdir -p $PREFIX/bin
+cp clustering/* $PREFIX/bin
+cp scripts/* $PREFIX/bin
+chmod +x $PREFIX/bin/*
+
 pushd ./leafcutter
 # R refuses to build packages that mark themselves as Priority: Recommended
 mv DESCRIPTION DESCRIPTION.old
