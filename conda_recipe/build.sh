d6c7cc7b9d53a536270f0689747b867bf5adc0b1
Ulugbek Baymuradov <ulugbekbk@gmail.com>
2023-02-15 21:13:28 +0400

    temp: build with files

--- conda_recipe/build.sh
+++ conda_recipe/build.sh
@@ -2,12 +2,16 @@
 
 set -e -x
 
-pushd ./leafcutter
-# R refuses to build packages that mark themselves as Priority: Recommended
-mv DESCRIPTION DESCRIPTION.old
-grep -v '^Priority: ' DESCRIPTION.old >DESCRIPTION
+mkdir -p $PREFIX/bin
+cp clustering/* $PREFIX/bin
+cp scripts/* $PREFIX/bin
 
-$R -e "install.packages('TailRank', repos='http://R-Forge.R-project.org')
-"
-$R CMD INSTALL --build .
-popd
+# pushd ./leafcutter
+# # R refuses to build packages that mark themselves as Priority: Recommended
+# mv DESCRIPTION DESCRIPTION.old
+# grep -v '^Priority: ' DESCRIPTION.old >DESCRIPTION
+
+# $R -e "install.packages('TailRank', repos='http://R-Forge.R-project.org')
+# "
+# $R CMD INSTALL --build .
+# popd
